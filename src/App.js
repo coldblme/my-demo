6abb456c54cc90529f830a919a7e38f308623e31
liuyi <yi.liu@wallyt.com>
2021-01-18 19:53:23 +0800

    feat: build project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {render} from 'react-dom';
+// import Router from './router/router';//加载路由文件
 
-class App extends React{
+class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      // <Router />
+      <div>12</div>
     )
   }
 }
